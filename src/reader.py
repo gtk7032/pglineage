8bcadfd5963a7ebe27c150740b3128430628aea5
gtk7032 <gtk7032@gmail.com>
2023-07-02 20:05:49 +0900

    wip

--- src/reader.py
+++ src/reader.py
@@ -6,15 +6,19 @@ from typing import Tuple
 class FileReader:
 
     def __init__(self) -> None:
-        self.p = re.compile(
-            "((?:with|select|update|insert|delete).+?;)",
+        self.p1 = re.compile(
+            "(?:with|select|update|insert|delete).+?;",
             flags=re.IGNORECASE + re.DOTALL,
         )
+        self.p2 = re.compile("-- .*?\n", flags=re.DOTALL)
+        self.p3 = re.compile("/\*.*\*/", flags=re.DOTALL)
 
     def read(self, path:str)->list[Tuple[str,str]]:
         with open(path,"r") as f:
             s = f.read().lower()
-        sqls = self.p.findall(s)
+        s = self.p2.sub("", s)
+        s = self.p3.sub("", s)
+        sqls = self.p1.findall(s)
         name, _ = os.path.splitext(os.path.basename(path))
         name=name.lower()
         return [ (name+"-"+str(i+1), sql) for i, sql in enumerate(sqls)]
