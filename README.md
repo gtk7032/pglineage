b015ef74be24aa9a661ecc9ab7107f57b96b7044
gtk7032 <gtk7032@gmail.com>
2023-07-09 17:45:40 +0900

    wip

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # pglineage
+
+## Overview
+pglineage is a tool to create data flow diagrams (column-level and table-level) in PostgreSQL by analyzing SQL.
+
+## Installation
+To use pglineage, follow these steps:
+
+1. Download:
+    ~~~bash
+    git clone https://github.com/gtk7032/pglineage.git
+    ~~~  
+
+1. Build the Docker image:
+    ~~~bash
+    docker-compose build --build-arg UID="`id -u`" --build-arg GID="`id -g`"
+    ~~~
+    The UID and GID arguments will be assigned to the user used within the container.
+
+1. Launch the container:
+
+    ~~~bash
+    docker-compose up -d
+    ~~~
+
+## Usage
+
+1. Execute the sample program, which targets the SQL contained in all files within the resource folder:
+
+    ~~~bash
+    docker exec -it pglineage python src/sample.py
+    ~~~ 
+
+1. Verify that a data flow diagram has been generated in the output folder.
+
+    A file name with "tlv" indicates the table level, while "clv" indicates the column level.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Author
+[gtk7032](https://github.com/gtk7032)
+
+
+
+
--- resource/sample.sql
+++ resource/sample.sql
@@ -1,6 +1,61 @@
-WITH SELECT c1 FROM t1; SELECT 
-c2 FROM t2;
+-- WITH test1 AS
+--     (
+--     SELECT customer_id, SUM(amount) AS total_payment FROM payment 
+--     GROUP BY customer_id
+--     )
 
-SELECT fnk();
 
-insert into tgt (col1, col2) select col1, col2 from tgt where tgt.x = 2;
+-- SELECT test1.customer_id, test1.total_payment, customer.first_name
+-- FROM test1 
+-- INNER JOIN customer 
+-- ON test1.customer_id=customer.customer_id
+-- WHERE test1.total_payment>150;
+
+--  SELECT g.id, g.link, g.data, 1
+--     FROM graph g
+--   UNION ALL
+--     SELECT g.id, g.link, g.data, sg.depth + 1
+--     FROM graph g, search_graph sg
+--     UNION ALL
+--  SELECT gx.id, gx.link, gx.data, 1
+--     FROM bee gx;
+
+WITH RECURSIVE search_graph(id, link, data, depth) AS (
+    SELECT g.id, g.link, g.data, 1
+    FROM graph g
+  UNION ALL
+    SELECT g.id, g2.link, g.data, sg.depth + 1
+    FROM graph g, search_graph sg, graph2 g2
+    WHERE g.id = sg.link
+)
+SELECT id, link FROM search_graph;
+
+-- select distinct on (c1) c1, c2 from t1;
+
+-- WITH RECURSIVE r AS (
+--     SELECT
+--         projects.id as project_id,
+--         projects.pre_project_id as pre_project_id,
+--         projects.title as title,
+--         projects.id as base_project_id -- 集計元となるプロジェクトIDを取得する
+--     FROM
+--         projects
+--     WHERE
+--         projects.id <= 3 -- 結果が分かりやすいよう件数を制限
+--     UNION ALL
+--     SELECT
+--         projects.id as project_id,
+--         projects.pre_project_id as pre_project_id,
+--         projects.title as title,
+--         r.base_project_id as base_project_id -- WITH RECURSIVEのデータからデータを引き継ぐ
+--     FROM
+--         projects,r
+--     WHERE
+--        projects.id = r.pre_project_id -- ここで再帰的にデータを紐づけ
+-- )
+
+-- SELECT
+--     project_id
+-- FROM
+--     r
+-- ORDER BY base_project_id ASC;
